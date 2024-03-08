d5b94274336dd072b276388b8f7e7648e0ea5178
ALW-N <officialalwintomy@gmail.com>
2024-03-09 04:25:50 +0530

    Navigation Setup

--- App.js
+++ App.js
@@ -1,60 +1,12 @@
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faUser, faCalendarAlt, faPlusCircle,  faTasks } from '@fortawesome/free-solid-svg-icons'; 
-
-// Import your screens
-import Home from './src/screens/home';
-import Profile from './src/screens/profile';
-import Calendar from './src/screens/calendar';
-import Task from './src/screens/task'; // Import the Task screen
-
-const Tab = createBottomTabNavigator();
+import BottomTabNavigator from './src/navigation/BottomTabNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused }) => {
-            let iconName;
-            let color; // Define color variable
-            switch (route.name) {
-              case 'Home':
-                iconName = faPlusCircle;
-                break;
-              case 'Profile':
-                iconName = faUser;
-                break;
-              case 'Calendar':
-                iconName = faCalendarAlt;
-                break;
-              case 'Task':
-                iconName = faTasks; // Use checklist icon for Task screen
-                break;
-              default:
-                iconName = faPlusCircle;
-                break;
-            }
-            color = focused ? '#DDFF94' : '#748c94'; // Set color based on focus
-            // Use the color variable for the icon color
-            return <FontAwesomeIcon icon={iconName} size={24} color={color} />;
-          },
-          tabBarShowLabel: false,
-          tabBarStyle: {
-            backgroundColor: '#000000', // Set the background color to black
-            height: 60,
-          },
-          headerShown: false,
-        })}
-      >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Task" component={Task} />
-        <Tab.Screen name="Calendar" component={Calendar} />
-        <Tab.Screen name="Profile" component={Profile} />
-      </Tab.Navigator>
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 }
---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,26 @@
+// src/navigation/AppNavigator.js
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../screens/home';
+import Profile from '../screens/profile';
+import Calendar from '../screens/calendar';
+import AddTaskScreen from '../screens/AddTaskScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" headerMode="none">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Calendar" component={Calendar} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="AddTaskScreen" component={AddTaskScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
